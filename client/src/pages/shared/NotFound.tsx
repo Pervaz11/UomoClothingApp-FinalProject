6e8f3267009bde5c5248be4563bef7cddbaf3036
Pervaz11 <parvazsa@code.edu.az>
2025-09-21 22:02:30 +0400

    fix(404-page): make Not Found page fully responsive

--- client/src/pages/shared/NotFound.tsx
+++ client/src/pages/shared/NotFound.tsx
@@ -15,8 +15,8 @@ const NotFound = () => {
             >
                 <div className="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
                     <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-black">404</p>
-                    <p className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-500 mt-2">Page Not Found</p>
-                    <p className="text-lg md:text-xl lg:text-2xl text-gray-500 my-12">Sorry, the page you are looking for could not be found.</p>
+                    <p className="text-3xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-500 mt-2">Page Not Found</p>
+                    <p className="text-lg w-55 md:text-xl lg:text-2xl text-gray-500 my-12">Sorry, the page you are looking for could not be found.</p>
                     <Link
                         to="/"
                         className="flex items-center space-x-2 bg-[#ef5c22] text-gray-100 px-4 py-2 rounded-full transition duration-300 ease-in-out transform hover:scale-105 hover:bg-gradient-to-r hover:from-[#ef5c22] hover:to-[#ff7f50] shadow-lg hover:shadow-xl hover:rotate-2"
